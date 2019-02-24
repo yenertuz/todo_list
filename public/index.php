4c85ea3bb85ecffd5d6634017ccffce3ae84a20c
yenertuz <yenertuz@hotmail.com>
2019-02-23 19:45:54 -0800

    update index.php

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
 define('LARAVEL_START', microtime(true));
 
